195942538e8cfd3b45efc5f9eda1a82a643ac480
M4nibarca <166192247+M4nibarca@users.noreply.github.com>
2024-07-22 19:40:48 +0530

    Add files via upload
    Solving each and every code which is available in the book Let Us Code by Yashavant Kanetkar 16th edition
    going over the basics again and working on improving the knowledge of c.

---
+++ JustForFun.c
@@ -0,0 +1,13 @@
+/*Just for Fun */
+#include <stdio.h>
+int main()
+{
+    int num;
+    printf("enter a number ");
+    scanf("%d",&num);
+
+    printf("Now I am letting you on a secret...\n");
+    printf("you have just entered the number %d\n",num);
+    return 0;
+
+}
